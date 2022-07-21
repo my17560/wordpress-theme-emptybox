45acfaea675c81cf8e01de2836c43c6c8f7a4f50
Masaki Yasutake <my17560@gmail.com>
2022-07-21 16:49:01 +0900

    Fixed to use get_template_directory() instead of get_stylesheet_directory().

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 // =============================================================================
 
 // Init EmptyBox
-require_once(get_stylesheet_directory() . '/inc/template_init.php');
-require_once(get_stylesheet_directory() . '/inc/template_functions.php');
+require_once(get_template_directory() . '/inc/template_init.php');
+require_once(get_template_directory() . '/inc/template_functions.php');
 
 // Widgets
-require_once(get_stylesheet_directory() . '/inc/widgets/emptybox_basicparts.php');
+require_once(get_template_directory() . '/inc/widgets/emptybox_basicparts.php');
