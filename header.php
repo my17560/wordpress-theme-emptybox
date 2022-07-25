16a267e0abf465f88590c3e39906296ee0261685
Masaki Yasutake <my17560@gmail.com>
2022-07-25 11:40:28 +0900

    Fixed passing missing parameters.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
-	<?php get_template_part('template-widgets/head-utils', $args["subType"], $args); ?>
+	<?php get_template_part('template-widgets/head-utils'); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="site">
